21cad7b9e0615c0cce6eec4170d7aedca4657d6e
lesenv <some123stin@gmail.com>
2025-03-16 20:49:48 +0100

    iterating Block!

--- Tetris new refactored with test/logic.py
+++ Tetris new refactored with test/logic.py
@@ -40,6 +40,7 @@ setting global constants for easily using in methods
 MOVE_DOWN = "fall"
 MOVE_LEFT = "left"
 MOVE_RIGHT = "right"
+MOVE_TURN = "turn"
 
 Z_BLOCK = [[1,1,0],[0,1,1]]
 S_BLOCK = [[0,1,1],[1,1,0]]
@@ -91,7 +92,12 @@ class Block():
             else:
                 rotated = list(zip(*self.block[::-1]))
                 self.block = list(list(x) for x in rotated)
-            return 
+            return
+
+        def __iter__(self):
+            for i in range(self.get_height()):
+                for j in range(self.get_width()):
+                    yield i, j, self.block[i][j]
             
 #        def __repr__(self):
 #            string = "\n".join(self.block)
@@ -166,7 +172,7 @@ class Playscreen():
             #Exception and lose game
 
     def get_block_pos(self, block, abs = True):
-    '''DOESN'T WORK'''
+        '''DOESN'T WORK yet'''
         x, y = self.active_block_pos
         for i in range(block.get_width()):
             for j in range(block.get_width()):
@@ -189,7 +195,7 @@ class Playscreen():
                 _x = x + i
                 _y = y + j
                 self.playmatrix[_x][_y] = block.block[j][i]
-          print("alt:", self.playmatrix)
+#          print("alt:", self.playmatrix)
                 
     def erase_active_block(self):
           block = self.active_block
@@ -245,9 +251,13 @@ class Playscreen():
 
     def turn(self):
         dummy = self.active_block
+        absx, absy = self.active_block_pos
         dummy.turn()
-        for row in dummy.block:
-            print(row)
+        for j, row in dummy.block:#
+            for i, el in row:
+                if dummy[i][j] and self.playmatrix[absx+i][absy+j]:
+                    raise BlockBlockedError("wanting to turn but some Block is in the way")
+        self.active_block.turn()
             
     def move(self, direction):
             before = self.counting(self.playmatrix)
@@ -261,6 +271,8 @@ class Playscreen():
                         self.go_left()
                     elif direction == MOVE_RIGHT:
                         self.go_right()
+                    elif direction == MOVE_TURN:
+                        self.turn()
             except BlockTooRightError:
                 self.active_block_pos[1] = self.get_width()-self.active_block.get_width()-1
             except BlockTooLeftError:
@@ -291,17 +303,17 @@ if __name__ == "__main__":
     print("printing")
     tetris.print_me()
     print("Fall twice")
-    tetris.fall_down()
-    tetris.fall_down()
+    tetris.move(MOVE_DOWN)
+    tetris.move(MOVE_DOWN)
     tetris.print_me()
     print("go right once")
-    tetris.go_right()
+    tetris.move(MOVE_RIGHT)
     tetris.print_me()
     print("go left twice")
-    tetris.go_left()
-    tetris.go_left()
+    tetris.move(MOVE_LEFT)
+    tetris.move(MOVE_LEFT)
     tetris.print_me()
     print("turn")
-    tetris.turn()
+    tetris.move(MOVE_TURN)
     tetris.print_me()
     
--- Tetris new refactored with test/test_logic.py
+++ Tetris new refactored with test/test_logic.py
@@ -1,17 +1,13 @@
+from sre_constants import ANY
 import unittest
 import logic
 
 class Test_Blocks(unittest.TestCase):
     def setUp(self):
         # Blocks
-        self.threex1 = logic.Block([3,3,3])
+        self.threex1 = logic.Block([[3,3,3]])
         self.fourx3 = logic.Block([[4,4,4,4],[4,4,4,4],[4,4,4,4]])
         self.onex3 = logic.Block([[3],[3],[3]])
-# to test:
-              ### Block
-              ## getwidth()
-              ## getheight()
-              ## turn()
 
     def test_Block_getwidth(self):
         self.assertEqual(self.threex1.get_width(), 3)
@@ -22,6 +18,23 @@ class Test_Blocks(unittest.TestCase):
         self.assertEqual(self.threex1.get_height(), 1)
         self.assertEqual(self.onex3.get_height(), 3)
         self.assertEqual(self.fourx3.get_height(), 3)
+
+    def test_Block_iterate(self):
+        # normal matrix
+        result = logic.Block([[0,0,0,0],[1,2,3,4],[None, ANY, -1, "wer"]])
+        for i, j, res in self.fourx3:
+            result.block[i][j] = res
+        self.assertEqual(result.block, self.fourx3.block, "iterating doesn't work")
+        # matrix with width 1
+        result = logic.Block([[None], [ANY],[ "wer"]])
+        for i, j, res in self.onex3:
+            result.block[i][j] = res
+        self.assertEqual(result.block, self.onex3.block, "iterating doesn't work")
+        # matrix with height 1
+        result = logic.Block([[1, ANY,  "wer"]])
+        for i, j, res in self.threex1:
+            result.block[i][j] = res
+        self.assertEqual(result.block, self.threex1.block, "iterating doesn't work")
         
 
 class Test_Moving_Tiles(unittest.TestCase):
