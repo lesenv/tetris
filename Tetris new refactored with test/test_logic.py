d0387de552dd3064e90ebf4f0603e3073f15c0af
lesenv <some123stin@gmail.com>
2025-05-09 21:54:00 +0200

    deleted test_default_block(), because it was duplicated with test_setup()
    test_block_move_right() see issues today

--- Tetris new refactored with test/test_logic.py
+++ Tetris new refactored with test/test_logic.py
@@ -13,10 +13,11 @@ Z_BLOCK = [[0,1,1],
 class Test_Moving_Tiles(unittest.TestCase):
     def setUp(self):
         # playscreens
+        # create a playscreen, 4x4 playmatrix, first Block is the S_BLOCK
         self.playscreen4x4s = logic.Playscreen(4,4, block = logic.Block(S_BLOCK))
-        #self.playscreen4x4s.new_Block(self.S_BLOCK)
+        # the other playscreen analogous  to the first one
         self.playscreen10x10z = logic.Playscreen(10, 10, block = logic.Block(Z_BLOCK))
-        #self.playscreen10x10z.new_Block(self.Z_BLOCK)
+        # one with non-squared playmatrix
         self.playscreen3x10z = logic.Playscreen(3, 10, block = logic.Block(Z_BLOCK))
 
     def test_setup(self):
@@ -31,55 +32,94 @@ class Test_Moving_Tiles(unittest.TestCase):
                      [0,1,0,0,0],
                      [0,0,0,0,0]]
         self.assertEqual(logic.Playscreen(5,4,block=logic.Block(Z_BLOCK)).playmatrix, test_5x4z,"5x4z didn't work")
+
+        test_3x10z = [[0,1,0],
+                      [1,1,0],
+                      [1,0,0],
+                      [0,0,0],
+                      [0,0,0],
+                      [0,0,0],
+                      [0,0,0],
+                      [0,0,0],
+                      [0,0,0],
+                      [0,0,0]]
+        self.assertEqual(self.playscreen3x10z.playmatrix, test_3x10z, "3x10z doesn't work")
                                                                                           
     def test_turning_block(self):
         #
         # since we're testing logic.py, use MOVE_TURN!!
         # 
 
-        self.playscreen4x4s.move(logic.MOVE_TURN)
         test_4x4s_one_turn = [[0,0,1,1],
                               [0,1,1,0],
                               [0,0,0,0],
                               [0,0,0,0]]
-        self.assertEqual(self.playscreen4x4s.playmatrix, test_4x4s_one_turn, "one turn of 4x4s")
-
         self.playscreen4x4s.move(logic.MOVE_TURN)
+        self.assertEqual(self.playscreen4x4s.playmatrix, test_4x4s_one_turn, "one turn of 4x4s")
+        # second turn, asserting turned back
         test_4x4s_two_turns = [[0,1,0,0],
                                [0,1,1,0],
                                [0,0,1,0],
                                [0,0,0,0]]
+        self.playscreen4x4s.move(logic.MOVE_TURN)
         self.assertEqual(self.playscreen4x4s.playmatrix, test_4x4s_two_turns, "two turns of 4x4s")
         
+
+        test_3x10z_one_turn = [[1,1,0],
+                               [0,1,1],
+                               [0,0,0],
+                               [0,0,0],
+                               [0,0,0],
+                               [0,0,0],
+                               [0,0,0],
+                               [0,0,0],
+                               [0,0,0],
+                               [0,0,0]]
         self.playscreen3x10z.move(logic.MOVE_TURN)
-        ##
-        # insert assert here
-        ###
+        self.assertEqual(self.playscreen3x10z.playmatrix, test_3x10z_one_turn, "one turn of 3x10z")
+        
+        test_3x10z_two_turns = [[0,1,0],
+                                [1,1,0],
+                                [1,0,0],
+                                [0,0,0],
+                                [0,0,0],
+                                [0,0,0],
+                                [0,0,0],
+                                [0,0,0],
+                                [0,0,0],
+                                [0,0,0]]
         self.playscreen3x10z.move(logic.MOVE_TURN)
+        self.assertEqual(self.playscreen3x10z.playmatrix, test_3x10z_two_turns, "second turn of 3x10z")
 
 
     def test_zero_matrix(self):
         self.assertEqual(logic.zero_matrix(2,2), [[0,0],[0,0]], "2-2 0-matrix didn't work")
         self.assertEqual(logic.zero_matrix(3,4), [[0,0,0],[0,0,0],[0,0,0],[0,0,0]], "3-4 0_matrix didn't work")
         
-    def test_default_block(self):
-        #default_start_playscreen = dsp
-        dsp = logic.zero_matrix(4, 4)
-        dsp[0][1] = 1
-        dsp[1][2], dsp[1][1] = 1, 1
-        dsp[2][2] = 1
-        self.assertEqual(self.playscreen4x4s.playmatrix, dsp, "playscreen didn't equal to 4x4-0-matrix")
-        
     def test_fall_block(self):
         #fallen_playscreen = fp
-        fp = logic.zero_matrix(4, 4)
-        fp[1][1] = 1
-        fp[2][2], fp[2][1] = 1, 1
-        fp[3][2] = 1
+        fp = [[0,0,0,0],
+              [0,1,0,0],
+              [0,1,1,0],
+              [0,0,1,0]]
         self.playscreen4x4s.move(logic.MOVE_DOWN)
-        self.assertEqual(self.playscreen4x4s.playmatrix, fp , "fell not okay")
+        self.assertEqual(self.playscreen4x4s.playmatrix, fp , "4x4s fell not okay")
+
+        fp = [[0,0,0],
+              [0,1,0],
+              [1,1,0],
+              [1,0,0],
+              [0,0,0],
+              [0,0,0],
+              [0,0,0],
+              [0,0,0],
+              [0,0,0],
+              [0,0,0]]
+        self.playscreen3x10z.move(logic.MOVE_DOWN)
+        self.assertEqual(self.playscreen3x10z.playmatrix, fp, "3x10z didn't fall alright") #sometimes, but most times it kicks you in the teeth
 
     def test_falling_block_collided_with_existing_block(self):
+        # think of what is happening
         for _ in range(3):
             self.playscreen10x10z.move(logic.MOVE_DOWN)
         self.playscreen10x10z.new_Block()
@@ -91,7 +131,13 @@ class Test_Moving_Tiles(unittest.TestCase):
         self.playscreen4x4s.move(logic.MOVE_DOWN)
         bottom_screen = self.playscreen4x4s.playmatrix
         self.playscreen4x4s.move(logic.MOVE_DOWN)
-        self.assertEqual(self.playscreen4x4s.playmatrix, bottom_screen, "Block moved, although Block is at the bottom")
+        self.assertEqual(self.playscreen4x4s.playmatrix, bottom_screen, "4x4s: Block moved, although Block is at the bottom")
+
+        for _ in range(10):
+            self.playscreen3x10z.move(logic.MOVE_DOWN)
+        bottom_screen = self.playscreen3x10z.playmatrix
+        self.playscreen3x10z.move(logic.MOVE_DOWN)
+        self.assertEqual(self.playscreen3x10z.playmatrix, bottom_screen, "3x10z: Block moved, although Block is at the bottom")
 
     def test_block_move_right(self):
         test = logic.zero_matrix(10,10)
@@ -106,6 +152,20 @@ class Test_Moving_Tiles(unittest.TestCase):
         test[2][6] = 1
         self.playscreen10x10z.move(logic.MOVE_RIGHT)
         self.assertEqual(self.playscreen10x10z.playmatrix, test, "didn't move right accordingly second time")
+
+        
+        test = logic.zero_matrix(3,10)
+        test[0][2] = 1
+        test[1][1], test[1][2] = 1,1
+        test[2][1] = 1
+        self.playscreen3x10z.move(logic.MOVE_RIGHT)
+        self.assertEqual(self.playscreen3x10z.playmatrix, test, "didn't move right accordingly second time")
+        test = logic.zero_matrix(3,10)
+        test[0][2] = 1
+        test[1][1], test[1][2] = 1,1
+        test[2][1] = 1
+        self.playscreen3x10z.move(logic.MOVE_RIGHT)
+        self.assertEqual(self.playscreen3x10z.playmatrix, test, "didn't move right accordingly second time")
 
     def test_block_too_right_at_the_right_side(self):
         #for _ in range(3):
